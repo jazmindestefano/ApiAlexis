d094b7795da2c87010889b843e2de2ec59a90878
Jazmin De Stefano <jazminisabeldestefano@gmail.com>
2023-02-08 15:41:27 -0300

    implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form.component.jsx'
+import {Flex} from '@chakra-ui/react'
+import Results  from './components/Results.component';
+import { useEffect, useState } from 'react';
+import { getResults } from './utils/result';
 
 function App() {
+
+  const [result, setResult] = useState(null)
+
+  useEffect(() => {
+    const apiResult = getResults()
+    apiResult.then((value) => {
+      setResult(value)
+    });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><Flex w={'100vw'} h={'100vh'} align={'center'} justify={'center'} direction={'column'}>
+      <Form />
+      <Results result={result} />
+    </Flex></>
   );
 }
 
