10b401dab24fca28b63419af00d4b2bb43022bbe
Jazmin De Stefano <jazminisabeldestefano@gmail.com>
2023-02-08 15:59:19 -0300

    style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Form from './components/Form.component.jsx'
-import {Flex} from '@chakra-ui/react'
+import {Flex, Stack} from '@chakra-ui/react'
 import Results  from './components/Results.component';
 import { useEffect, useState } from 'react';
 import { getResults } from './utils/result';
@@ -17,8 +17,9 @@ function App() {
   }, [])
 
   return (
-    <><Flex w={'100vw'} h={'100vh'} align={'center'} justify={'center'} direction={'column'}>
+    <><Flex w={'100vw'} h={'100vh'} align={'center'} justify={'center'} direction={'column'} gap={50}>
       <Form />
+      <Stack h={'1px'} w={'100%'} bgColor={'black'}></Stack>
       <Results result={result} />
     </Flex></>
   );
--- src/components/Results.component.js
+++ src/components/Results.component.js
@@ -1,32 +1,29 @@
-import { VStack, Text } from '@chakra-ui/react'
+import { VStack, Text, Stack } from '@chakra-ui/react'
 
 
 const Results = ({result}) => {
 
-    console.log(result.abilities.map((item) => item.ability.name))
-
     return (
-        <VStack w={'80%'} bgColor={'lightblue'}>
-            <Text>Api Results</Text>
-            <VStack w={'100%'}>
-            {
-                result.abilities?.map((item) => 
-                {
+        
+        <VStack w={'80%'}>
+            <Text>Pokemon Results</Text>
+            <Stack w={'100%'} direction={'row'} justify={'center'}>
+                {result.abilities?.map((item) => {
                     return (
                         <>
-                        
-                        <Text>
-                            {item.ability.name}
-                        </Text>
-                        <Text>
-                            {item.slot}
-                        </Text>
+                            <Stack width={'200px'} h={'200px'} bgColor={'blue'} align={'center'} justify={'center'} borderRadius={'15px'}>
+                                <Text color={'white'} fontWeight={500}>
+                                    AbilityName: {item.ability.name}
+                                </Text>
+                                <Text color={'white'} fontWeight={500}>
+                                    Slot: {item.slot}
+                                </Text>
+                            </Stack>
                         </>
                     )
                 }
-                )
-            }
-            </VStack>
+                )}
+            </Stack>
         </VStack>
     )
 }
